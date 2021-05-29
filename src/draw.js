37f4e22db5c9e36010b85e8d7635be6d760b09d9
Jacob Allen <enoua5@gmail.com>
2021-05-28 20:42:37 -0600

    Add ice settings

--- src/draw.js
+++ src/draw.js
@@ -33,18 +33,39 @@ function draw(canvas, gens, settings)
 {
   let ctx = canvas.getContext('2d');
   
-  let newFrame = ctx.createImageData(canvas.width, canvas.height);
+  let h = canvas.height;
+  let w = canvas.width;
+  
+  let newFrame = ctx.createImageData(w, h);
   let i = 0;
   
-  for(let y = 0; y < canvas.height; y++)
+  
+  for(let y = 0; y < h; y++)
   {
-    for(let x = 0; x < canvas.width; x++)
+    for(let x = 0; x < w; x++)
     {
       let coord = GlobeNoise.pix_to_coord(x, y, settings.width, settings.height);
       let lat = coord.lat;
-      let lon = coord.lon
-      let height = gens.height.getValue(lat, lon, settings.height_octaves)
+      let lon = coord.lon;
+      let height = gens.height.getValue(lat, lon, settings.height_octaves);
+      
+      
       let heat = gens.heat.getValue(lat, lon, settings.heat_octaves);
+      // 0 = at pole, 1 = at equator
+      let distanceToPole = 1-(2/h)*Math.abs(y-h/2);
+      // if in icecap range...  
+      if(distanceToPole < settings.ice/100)
+      {
+        // refit to a pole-to-edge-of-ice
+        let distanceIntoIcecap = 1-refit(distanceToPole, 0, settings.ice/100, 0, 1);
+        heat -= distanceIntoIcecap * settings.iceStrength;
+      }
+      if(height > settings.iceAltitude)
+      {
+        heat -= settings.iceAltitudeStrength;
+      }
+      
+      
       let humidity = gens.humidity.getValue(lat, lon, settings.humidity_octaves);
       
       let color = getColor(height, heat, humidity, settings);
