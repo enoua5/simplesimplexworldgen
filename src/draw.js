5d2db182a59bf86a09030eb8e982112217e35ae8
Jacob Allen <enoua5@gmail.com>
2021-05-29 17:57:01 -0600

    Add loading indicator
    While it *does* make it run slower, it helps to see progress be made, and also keeps the page from hanging

--- src/draw.js
+++ src/draw.js
@@ -46,6 +46,62 @@ function getColor(height, heat, humidity, settings)
 //  return {red: heat, green: height, blue: humidity};
 }
 
+function get3h(coords, gens, settings) // 3h = Height, Heat, Humidity
+{
+  let lat = coords.lat;
+  let lon = coords.lon;
+  
+  let height = gens.height.getValue(lat, lon, settings.height_octaves) + settings.height_bias;
+  
+  let heat = gens.heat.getValue(lat, lon, settings.heat_octaves) + settings.heat_bias;
+  // -1 = at pole, 0 = 45th parallel, 1 = at equator
+  let latHeatMult = Math.cos(2*lat);
+  heat -= latHeatMult*settings.latHeat;
+  
+  if(height > settings.iceAltitude)
+  {
+    heat -= settings.iceAltitudeStrength;
+  }
+  
+  let humidity = gens.humidity.getValue(lat, lon, settings.humidity_octaves) + settings.humidity_bias;
+  
+  return {height, heat, humidity};
+}
+
+function drawLine(ctx, frame, y, gens, settings)
+{
+  let w = frame.width;
+  let i = 4 * w * y;
+  
+  for(let x = 0; x < w; x++)
+  {
+    let coord = GlobeNoise.pix_to_coord(x, y, settings.width, settings.height);
+    let hhh = get3h(coord, gens, settings);
+    
+    let color = getColor(hhh.height, hhh.heat, hhh.humidity, settings);
+    
+    frame.data[i++] = color.red;
+    frame.data[i++] = color.green;
+    frame.data[i++] = color.blue;
+    frame.data[i++] = 255; // alpha
+  }
+  
+  if(y + 1 >= frame.height)
+  {
+    document.getElementById("submit_button").disabled = false;
+    document.getElementById("submit_button").innerText = "Redraw";
+    ctx.putImageData(frame, 0, 0);
+  }
+  else
+  {
+    document.getElementById("submit_button").innerText = Math.floor(100*y/frame.height) + '%';
+    // to avoid hanging the page
+    setTimeout(function(){
+      drawLine(ctx, frame, y+1, gens, settings);
+    },0);
+  }
+}
+
 function draw(canvas, gens, settings)
 {
   let ctx = canvas.getContext('2d');
@@ -56,38 +112,7 @@ function draw(canvas, gens, settings)
   let newFrame = ctx.createImageData(w, h);
   let i = 0;
   
-  
-  for(let y = 0; y < h; y++)
-  {
-    for(let x = 0; x < w; x++)
-    {
-      let coord = GlobeNoise.pix_to_coord(x, y, settings.width, settings.height);
-      let lat = coord.lat;
-      let lon = coord.lon;
-      let height = gens.height.getValue(lat, lon, settings.height_octaves) + settings.height_bias;
-      
-      
-      let heat = gens.heat.getValue(lat, lon, settings.heat_octaves) + settings.heat_bias;
-      // -1 = at pole, 0 = 45th parallel, 1 = at equator
-      let latHeatMult = Math.cos(2*lat);
-      heat -= latHeatMult*settings.latHeat;
-      
-      
-      if(height > settings.iceAltitude)
-      {
-        heat -= settings.iceAltitudeStrength;
-      }
-      
-      
-      let humidity = gens.humidity.getValue(lat, lon, settings.humidity_octaves) + settings.humidity_bias;
-      
-      let color = getColor(height, heat, humidity, settings);
-      
-      newFrame.data[i++] = color.red;
-      newFrame.data[i++] = color.green;
-      newFrame.data[i++] = color.blue;
-      newFrame.data[i++] = 255; // alpha
-    }
-  }
-  ctx.putImageData(newFrame, 0, 0)
+  document.getElementById("submit_button").disabled = true;
+  // each calls the next line using setTimout to avoid hangs
+  drawLine(ctx, newFrame, 0, gens, settings);
 }
